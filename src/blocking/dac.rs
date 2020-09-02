dc183b53c5b23145545fd6b8ec71cc6ef1f4369a
Mendelt Siebenga <msiebenga@gmail.com>
2020-09-02 17:35:06 +0200

    make Word an associated type instead of generic type parameter

--- src/blocking/dac.rs
+++ src/blocking/dac.rs
@@ -2,13 +2,16 @@
 
 /// A single DAC channel. Word is the type used to represent a single sample, this would typically
 /// be u8, u16 or u32.
-/// Note that not all bits will always be used. A 12 bit DAC for example will probably use u16 here
-/// <DISCUSSION> should we prescribe to use the most significant bits here for compat between word
-///   sizes?
-pub trait DAC<WORD> {
+/// Note that not all bits will always be used. A 12 bit DAC for example will use u16. In this case
+/// the DAC should use the most significant bits and ignore any unused bits to make it easier
+/// to exchange dacs with the same Word length but different numbers of bits.
+pub trait DAC {
     /// Error type returned by DAC methods
     type Error;
 
+    /// Word length, should be u8, u16 or u32 depending on precision of the DAC
+    type Word;
+
     /// Set the output of the DAC
-    fn try_set_output(&mut self, value: WORD) -> Result<(), Self::Error>;
+    fn try_set_output(&mut self, value: Self::Word) -> Result<(), Self::Error>;
 }
